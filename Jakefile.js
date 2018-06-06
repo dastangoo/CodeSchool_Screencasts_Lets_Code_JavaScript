9b8f80fc97c3e857c8744ea544796b823ebd22a9
dastangoo <hamed.dastangoo@gmail.com>
2018-06-07 01:02:49 +0430

    Using Jake for our build

---
+++ Jakefile.js
@@ -0,0 +1,16 @@
+(() => {
+  console.log("\n\nBUILD OK");
+  
+  // Default Task
+  
+  desc("Default build");
+  task("default", () => {
+    console.log("Hello, I'm the default task.");
+  });
+  
+  // Google Task
+  // desc("Pure ridiculosity");
+  // task("google", () => {
+  //   console.log("Hello, I'm the google task.");
+  // });
+})();
--- build.js
+++
@@ -1,3 +0,0 @@
-(() => {
-  console.log("\n\nBUILD OK");
-})();
