34941b87be87fb4623546e7995a35641f6762429
dastangoo <hamed.dastangoo@gmail.com>
2018-06-07 09:35:56 +0430

    Build script checks Node version

--- Jakefile.js
+++ Jakefile.js
@@ -1,16 +1,22 @@
 (() => {
   
-  // Default Task
+  const EXPECTED_NODE_VERSION = "v8.11.2";
   
   desc("Default build");
-  task("default", () => {
+  task("default", ["version"], () => {
     console.log("\n\nBUILD OK");
     console.log("Hello, I'm the default task.");
   });
   
-  // Google Task
-  // desc("Pure ridiculosity");
-  // task("google", () => {
-  //   console.log("Hello, I'm the google task.");
-  // });
+  desc("Check Node Version");
+  task("version", () => {
+    console.log("Checking Node version: .");
+  
+    const actualVersion = process.version;
+    if (actualVersion !== EXPECTED_NODE_VERSION) {
+      // This is a Jake function
+      fail(`Incorrect Node version: expected ${EXPECTED_NODE_VERSION}, but was ${actualVersion}`);
+    }
+  });
+  
 })();
